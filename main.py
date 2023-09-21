cc234bbb79d4fa914e7e63179057bcc2539a4952
Mikolaï Szychowski <szychowskim@educ.college-montreal.qc.ca>
2023-09-21 11:15:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#créé pâr Mikolai Szychowski
+#créé le 21 septembre 2023
+#TP1
+
+def count_word(mots): #definir la fonction
+    resultat=(len(mots.split(' ')))  #compter les mots
+    return resultat
